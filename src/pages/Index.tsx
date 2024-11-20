0f2f64887d581a58be9870a87565b074214105fc
hermes7144 <kichancho@gmail.com>
2024-11-20 16:53:54 +0900

    test

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,53 +1,59 @@
 import { useEffect } from 'react';
 import { handleAllowNotification } from '../service/notificationPermission';
-
+import axios from 'axios';
 
 function registerServiceWorker() {
-    if ("serviceWorker" in navigator) {
-        window.addEventListener("load", function () {
-            navigator.serviceWorker
-                .register("/firebase-messaging-sw.js")
-                .then(function (registration) {
-                    console.log(
-                        "Service Worker가 scope에 등록되었습니다.:",
-                        registration.scope
-                    );
-                })
-                .catch(function (err) {
-                    console.log("Service Worker 등록 실패:", err);
-                });
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+      navigator.serviceWorker
+        .register('/firebase-messaging-sw.js')
+        .then(function (registration) {
+          console.log('Service Worker가 scope에 등록되었습니다.:', registration.scope);
+        })
+        .catch(function (err) {
+          console.log('Service Worker 등록 실패:', err);
         });
-    }
+    });
+  }
 }
 
 export default function Index() {
-
   useEffect(() => {
     handleAllowNotification();
     registerServiceWorker();
   }, []);
 
-
-
-const handelClick = async () => {
+  const handelClick = async () => {
     // const addr = 'http://localhost:3000/send-notification';
     // const addr = 'http://localhost:8888/.netlify/functions/sendNotification';
-   const addr = 'https://fcm-server.netlify.app/.netlify/functions/sendNotification';
+    const addr = 'https://fcm-server.netlify.app/.netlify/functions/sendNotification';
 
-    await fetch(addr, {
-        method: 'POST',
+    const tokens = [
+      'dofRmJbOcmMHmeKjHLLrho:APA91bHwljA0shCtYWjxXQ1xgoyyii5CFoyLr2Q2y0R-R_9P8Ih-NcO-PRJe-KFBY7u2TyvFy7s0zECm6U9e7VU0xFlhgjDmT-dVyd2ZVYSrZjV3ozpx7Kw',
+      'eeyLuBmeQoMbXEcbbBcEqC:APA91bF6Xo57W_RvP6xVBLiQcVUP_B-Xqb-kEFr01rZusCJVuqjcNgHOvubj8Vy8Wjiu3QBCNVXOQ3adpBePxHctsC_DFCvN1M7UvyHMYGj-e5uK4uzPzHY',
+    ];
+    const payload = {
+      tokens: tokens,
+      title: '푸시 알림 제목',
+      body: '푸시 알림 본문 내용',
+    };
+    
+    try {
+      const response = await axios.post(addr, payload, {
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-        tokens: ["dofRmJbOcmMHmeKjHLLrho:APA91bHwljA0shCtYWjxXQ1xgoyyii5CFoyLr2Q2y0R-R_9P8Ih-NcO-PRJe-KFBY7u2TyvFy7s0zECm6U9e7VU0xFlhgjDmT-dVyd2ZVYSrZjV3ozpx7Kw",
-                "eeyLuBmeQoMbXEcbbBcEqC:APA91bF6Xo57W_RvP6xVBLiQcVUP_B-Xqb-kEFr01rZusCJVuqjcNgHOvubj8Vy8Wjiu3QBCNVXOQ3adpBePxHctsC_DFCvN1M7UvyHMYGj-e5uK4uzPzHY",
-        ],
-        title: '푸시 알림 제목',
-        body: '푸시 알림 본문 내용',
-        }),
-    });
-}
+      });
+      console.log('푸시 알림 성공:', response.data);
+    } catch (error) {
+      console.error('푸시 알림 실패:', error.response?.data || error.message);
+      console.log(error);
+      
+    }
+  };
 
-  return <>Index
-  <button onClick={handelClick}>버튼</button>
-  </>;
+  return (
+    <>
+      Index
+      <button onClick={handelClick}>버튼</button>
+    </>
+  );
 }
