b8a7e9aadad9d893ccfa9a5cb10f0d6f7787bceb
hermes7144 <kichancho@gmail.com>
2024-11-20 16:35:37 +0900

    test

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -30,7 +30,7 @@ export default function Index() {
 const handelClick = async () => {
     // const addr = 'http://localhost:3000/send-notification';
     // const addr = 'http://localhost:8888/.netlify/functions/sendNotification';
-    const addr = 'https://fcm-server.netlify.app/.netlify/functions/sendNotification';
+   const addr = 'https://fcm-server.netlify.app/.netlify/functions/sendNotification';
 
     await fetch(addr, {
         method: 'POST',
@@ -38,7 +38,6 @@ const handelClick = async () => {
         body: JSON.stringify({
         tokens: ["dofRmJbOcmMHmeKjHLLrho:APA91bHwljA0shCtYWjxXQ1xgoyyii5CFoyLr2Q2y0R-R_9P8Ih-NcO-PRJe-KFBY7u2TyvFy7s0zECm6U9e7VU0xFlhgjDmT-dVyd2ZVYSrZjV3ozpx7Kw",
                 "eeyLuBmeQoMbXEcbbBcEqC:APA91bF6Xo57W_RvP6xVBLiQcVUP_B-Xqb-kEFr01rZusCJVuqjcNgHOvubj8Vy8Wjiu3QBCNVXOQ3adpBePxHctsC_DFCvN1M7UvyHMYGj-e5uK4uzPzHY",
-                "cbx8GkbfRxSRgAMVILej9-:APA91bEajGelZHEUj0kWFOoZqOZGqxPP1455JfPKq6qy8q0JhzKp3bbs2lsXk81a0ncYogYxxfqHZr6Ea12s12XGSrHPUjzzSZLnCj4hgqS7WG7v0wJNznE"
         ],
         title: '푸시 알림 제목',
         body: '푸시 알림 본문 내용',
