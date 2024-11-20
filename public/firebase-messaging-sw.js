feb5ba01b33be2dd40160f1dd6641a733bc3c0b2
hermes7144 <kichancho@gmail.com>
2024-11-20 20:27:29 +0900

    test

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,28 +1,29 @@
 /* global importScripts */
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
 
-const config ={
-  apiKey: 'AIzaSyDRGQg2nWHBQjo6PTV9u51wp-J1hfD7y04',
-  authDomain: 'alrammate.firebaseapp.com',
-  projectId:'alrammate',
-  messagingSenderId:'alrammate.firebasestorage.app',
-  vapidKey: 'BCEqZPDQqPmm91G-bFFrsrCnnhH4rC-B4AxBkYVoJYb4Z90HYqZzzHJz82lQRMnaMBy84-LiAOGpuZkro3b-rQw',
-  appId: '1:393905672119:web:c96a80dd317149fb0fe232'
-};
+// const config ={
+//   apiKey: 'AIzaSyDRGQg2nWHBQjo6PTV9u51wp-J1hfD7y04',
+//   authDomain: 'alrammate.firebaseapp.com',
+//   projectId:'alrammate',
+//   messagingSenderId:'alrammate.firebasestorage.app',
+//   vapidKey: 'BCEqZPDQqPmm91G-bFFrsrCnnhH4rC-B4AxBkYVoJYb4Z90HYqZzzHJz82lQRMnaMBy84-LiAOGpuZkro3b-rQw',
+//   appId: '1:393905672119:web:c96a80dd317149fb0fe232'
+// };
 
-firebase.initializeApp(config);
+// firebase.initializeApp(config);
 
-self.addEventListener("install", function (e) {
-  self.skipWaiting();
-});
+// self.addEventListener("install", function (e) {
+//   self.skipWaiting();
+// });
 
-self.addEventListener("activate", function (e) {
-  console.log("fcm service worker가 실행되었습니다.");
-});
+// self.addEventListener("activate", function (e) {
+//   console.log("fcm service worker가 실행되었습니다.");
+// });
 
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
 
+// 왜인지 모르겠는데 이거 있으면 두번 호출된다
 // messaging.onBackgroundMessage((payload) => {  
 //   const notificationTitle = payload.notification.title
 //   const notificationOptions = {
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { handleAllowNotification, registerServiceWorker } from '../service/notificationPermission';
+// import { getMessaging, onMessage } from "firebase/messaging";
 
 
 
@@ -15,14 +16,14 @@ export default function Index() {
 const handelClick = async () => {
     // const addr = 'http://localhost:3000/send-notification';
     const addr = 'http://localhost:8888/.netlify/functions/sendNotification';
-//   const addr = 'https://fcm-server.netlify.app/.netlify/functions/sendNotification';
+  //const addr = 'https://fcm-server.netlify.app/.netlify/functions/sendNotification';
 
     await fetch(addr, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
         tokens: ["dofRmJbOcmMHmeKjHLLrho:APA91bHwljA0shCtYWjxXQ1xgoyyii5CFoyLr2Q2y0R-R_9P8Ih-NcO-PRJe-KFBY7u2TyvFy7s0zECm6U9e7VU0xFlhgjDmT-dVyd2ZVYSrZjV3ozpx7Kw",
-            "cbx8GkbfRxSRgAMVILej9-:APA91bGQyME5JkwGLAB8YJbkAZ94KP1s3gAeB5ABeELGLrcWQy7AZVayI-P0sm6Ot4QQqrcNwapPEqVhtFEJPD-sLTnje8h05_WQc3dY1_JdZn_VCR-HRVA",
+            "cbx8GkbfRxSRgAMVILej9-:APA91bFnhx9OWKzvgvffwC1EFbDSZ_z64Rby5H8HGECFjNKQanEhG0hHsbl5nunj3pfNwripN_9pHzl1r8uhLqWrytMDSU6-Cl6ZT3uSkbFm3iGxhPVyGOE",
             "eeyLuBmeQoMbXEcbbBcEqC:APA91bGpa9tOqNnteTQv1rKDwmjcmnWd370YGKgSUUaeOOq4LitGPFNDoEWAkJQdtv3bok4U29KvbZPwsrduuFoboS0iCKeEDFDTdVAzHn3ntMV6qX7b8dY",
 
         ],
