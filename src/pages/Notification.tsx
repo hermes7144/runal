da499bd3a185c563dc08ad832aff931de60e0318
hermes7144 <kichancho@gmail.com>
2024-11-25 21:42:34 +0900

    feat:wip

--- src/pages/Notification.tsx
+++ src/pages/Notification.tsx
@@ -5,36 +5,34 @@ import { useNavigate } from 'react-router-dom';
 import { useToastStore } from '../store/toastStore';
 
 function Notification() {
-  const { notificationQuery : { data: notification ,isLoading }, saveNotification} = UseNotification();
+  const {
+    notificationQuery: { data: notification, isLoading },
+    saveNotification,
+  } = UseNotification();
   const [regions, setRegions] = useState<string[]>(notification?.regions || []);
   const [events, setEvents] = useState<string[]>(notification?.events || []);
-    const { setToast } = useToastStore(); // useToastStore에서 setToast 가져오기
+  const { setToast } = useToastStore(); // useToastStore에서 setToast 가져오기
 
   const navigate = useNavigate();
-  
-    // notification 데이터가 변경되면 상태 업데이트
-    useEffect(() => {
-      if (notification) {
-        setRegions(notification.regions || []);
-        setEvents(notification.events || []);
-      }
-    }, [notification]);
 
-  const toggleSelection = (
-    item: string,
-    setSelectedItems: React.Dispatch<React.SetStateAction<string[]>>
-  ) => {
-    setSelectedItems((prev) =>
-      prev.includes(item) ? prev.filter((i) => i !== item) : [...prev, item]
-    );
+  // notification 데이터가 변경되면 상태 업데이트
+  useEffect(() => {
+    if (notification) {
+      setRegions(notification.regions || []);
+      setEvents(notification.events || []);
+    }
+  }, [notification]);
+
+  const toggleSelection = (item: string, setSelectedItems: React.Dispatch<React.SetStateAction<string[]>>) => {
+    setSelectedItems((prev) => (prev.includes(item) ? prev.filter((i) => i !== item) : [...prev, item]));
   };
 
   const handleSubmit = () => {
-      saveNotification.mutate({ regions, events });
+    saveNotification.mutate({ regions, events });
 
-      setToast('알림 설정이 저장되었습니다!');
-      navigate(-1)
-  }
+    setToast('알림 설정이 저장되었습니다!');
+    navigate(-1);
+  };
 
   return (
     <div className='p-6 max-w-lg mx-auto bg-white rounded-lg shadow-md'>
@@ -45,19 +43,22 @@ function Notification() {
         설정되지 않은 항목은 전체로 판단합니다.
       </p>
       {/* 장소 설정 */}
+
       <div className='my-6'>
+
+            <label className='cursor-pointer flex justify-between'>
+              <h3 className='font-semibold mb-2'>알림 기능 사용</h3>
+              <input type='checkbox' className='toggle toggle-primary' defaultChecked />
+            </label>
+            
         <h3 className='font-semibold mb-2'>지역</h3>
         <div className='flex flex-wrap gap-2'>
           {predefinedRegions.map((region) => (
-            <button
-              key={region}
-              className={`btn ${regions.includes(region) ? 'btn-primary btn-sm' : 'btn-outline btn-sm'}`}
-              onClick={() => toggleSelection(region, setRegions)}>
+            <button key={region} className={`btn ${regions.includes(region) ? 'btn-primary btn-sm' : 'btn-outline btn-sm'}`} onClick={() => toggleSelection(region, setRegions)}>
               {region}
             </button>
           ))}
         </div>
-        {/* 카테고리 설정 */}
         <div className='my-6'>
           <h3 className='font-semibold mb-2'>종목</h3>
           <div className='flex flex-wrap gap-2'>
@@ -70,9 +71,7 @@ function Notification() {
         </div>
       </div>
       {/* 저장 결과 확인 */}
-      <button
-        className='btn btn-success w-full text-white'
-        onClick={handleSubmit}>
+      <button className='btn btn-success w-full text-white' onClick={handleSubmit}>
         저장
       </button>
     </div>
