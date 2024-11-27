a7e32ea419db0aba0a317b02cc31db648591bdf3
hermes7144 <kichancho@gmail.com>
2024-11-27 16:42:53 +0900

    feat:allim

--- src/api/firebaseConfig.ts
+++ src/api/firebaseConfig.ts
@@ -6,11 +6,10 @@ import 'firebase/messaging';
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  vapidKey: import.meta.env.VITE_APP_VAPID_KEY,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
 
 };
 
