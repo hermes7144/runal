07a95d9e5b8fc4e3ba82a8be48457331e358f3bd
hermes7144 <kichancho@gmail.com>
2024-11-28 22:40:41 +0900

    data

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -24,11 +24,10 @@ const messaging = firebase.messaging();
 
 self.addEventListener('push', function(event) {
 
-  event.preventDefault();
   const message = event.data.json();  // FCM 메시지
-  const title = message.notification.title;
-  const body = message.notification.body;
-  const clickAction = 'https://runnings.netlify.app/';  // 기본 URL 설정
+  const title = message.data.title;
+  const body = message.data.body;
+  const clickAction = message.data.click_action;
 
   const options = {
     body: body,
