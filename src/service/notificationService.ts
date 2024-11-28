ef1336723e9f5908577dfe91b358aad6825d74f1
hermes7144 <kichancho@gmail.com>
2024-11-28 13:21:58 +0900

    feat:check

--- src/service/notificationService.ts
+++ src/service/notificationService.ts
@@ -52,9 +52,9 @@ export const requestNotificationPermission = async () => {
   try {
     const permission = await Notification.requestPermission();
     if (permission === "granted") {
-      console.log("알림 권한이 허용되었습니다.");
+      alert("알림 권한이 허용되었습니다.");
     } else {
-      console.log("알림 권한이 거부되었습니다.");
+      alert("알림 권한이 거부되었습니다.");
     }
   } catch (error) {
     console.error("알림 권한 요청 실패: ", error);
