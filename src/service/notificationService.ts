bd1e5d8e461b1078b473ea8b92d1b8a077227d57
hermes7144 <kichancho@gmail.com>
2024-12-02 22:14:02 +0900

    feat:sw-dev 고정

--- src/service/notificationService.ts
+++ src/service/notificationService.ts
@@ -3,15 +3,13 @@ import { fetchUsers } from '../api/database';
 export function registerServiceWorker() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-      const serviceWorkerFile =
-        import.meta.env.VITE_MODE === 'development'
-          ? '/firebase-messaging-sw-dev.js'  // 개발용
-          : '/firebase-messaging-sw.js';     // 배포용
+      // const serviceWorkerFile =
+      //   import.meta.env.VITE_MODE === 'development'
+      //     ? '/firebase-messaging-sw-dev.js'  // 개발용
+      //     : '/firebase-messaging-sw.js';     // 배포용
 
-      console.log(import.meta.env.VITE_MODE);
-      console.log(serviceWorkerFile);
-      
 
+      const serviceWorkerFile = '/firebase-messaging-sw-dev.js';
       navigator.serviceWorker
         .register(serviceWorkerFile)
         .then(function (registration) {
