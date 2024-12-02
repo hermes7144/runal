63040435c9ce76b7011c50ea81cbd1917ae51743
hermes7144 <kichancho@gmail.com>
2024-12-02 21:15:31 +0900

    feawt:확인

--- src/service/notificationService.ts
+++ src/service/notificationService.ts
@@ -8,7 +8,7 @@ export function registerServiceWorker() {
           ? '/firebase-messaging-sw-dev.js'  // 개발용
           : '/firebase-messaging-sw.js';     // 배포용
 
-      alert(serviceWorkerFile);
+      console.log(serviceWorkerFile);
       navigator.serviceWorker
         .register(serviceWorkerFile)
         .then(function (registration) {
