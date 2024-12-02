9aee6520833b6288612a30543fc6a7f86aea97bb
hermes7144 <kichancho@gmail.com>
2024-12-02 21:36:24 +0900

    feat:test

--- src/service/notificationService.ts
+++ src/service/notificationService.ts
@@ -7,9 +7,7 @@ export function registerServiceWorker() {
         import.meta.env.VITE_MODE === 'development'
           ? '/firebase-messaging-sw-dev.js'  // 개발용
           : '/firebase-messaging-sw.js';     // 배포용
-
-      console.log(import.meta.env.VITE_MODE);
-      console.log(serviceWorkerFile);
+          
       navigator.serviceWorker
         .register(serviceWorkerFile)
         .then(function (registration) {
